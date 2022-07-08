6d1bc50d0184b6c6510d5d4dcb77d15eb7c8e458
Alejandro Cortez <102841726+Muzan67@users.noreply.github.com>
2022-07-08 16:01:28 -0400

    updating assignment

---
+++ db/seeds.sql
@@ -0,0 +1,50 @@
+INSERT INTO id (id, department_id)
+VALUES 
+  (1,"Sales"),
+  (2,"Engineering"),
+  (3,"Finance"),
+  (4,"Legal"),
+  
+INSERT INTO role (id, title_id) 
+VALUES
+  (1,"Sales Lead"),
+  (2,"Salesperson"),
+  (3,"Lead Engineer"),
+  (4,"Software Engineer"),
+  (5. "Account Manager"),
+  (6. "Accountant"),
+  (7. "Legal Team Lead"),
+  (8. "Lawyer");
+
+INSERT INTO salary (id, salary_id) 
+VALUES
+  (1,"$100,000"),
+  (2,"$80,000"),
+  (3,"$150,000"),
+  (4,"$120,000"),
+  (5. "$160,000"),
+  (6. "$125,000"),
+  (7. "$250,000"),
+  (8. "$190,000");
+
+INSERT INTO employee (id, name_id, ) 
+VALUES 
+  (1,"John Doe"),
+  (2,"Mike Chan"),
+  (3,"Shley Rodriguez"),
+  (4,"Kevin Tupik"),
+  (5. "Kunal Singh"),
+  (6. "Malia Brown"),
+  (7. "Sarah Lourd"),
+  (8. "Tom Allen");
+
+INSERT INTO manager (id, manager_id, ) 
+VALUES 
+  (1,"null"),
+  (2,"John Doe"),
+  (3,"null"),
+  (4,"Ashley Rodriguez"),
+  (5. "null"),
+  (6. "Kunal Singh"),
+  (7. "null"),
+  (8. "Sarah Lourd");
