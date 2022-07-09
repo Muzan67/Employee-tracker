bbda38ea7efdcc5c28d9e5b242f12e2a1fb0ae23
Alejandro Cortez <102841726+Muzan67@users.noreply.github.com>
2022-07-09 09:31:06 -0400

    updating assignment

--- db/schema.sql
+++ db/schema.sql
@@ -4,21 +4,48 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department(
-  id INTEGER PRIMARY KEY,
-  department_name VARCHAR(30),
+ PRIMARY KEY (department_id),
+ role VARCHAR(30),
+ department_id INTEGER AUTO_INCREMENT
 );
+
+INSERT INTO department(id, role)
+VALUES 
+   (1,"Sales"),
+   (2,"Engineering"),
+   (3,"Finance"),
+   (4,"Legal");
 
 CREATE TABLE role(
-  id INTEGER PRIMARY KEY,
+  PRIMARY KEY (role_id),
   title VARCHAR(30),
   salary DECIMAL(10,2),
-  department_id INTEGER
+  department_id INTEGER,
+  role_id INTEGER AUTO_INCREMENT
 );
 
+INSERT INTO role (department_id, title, salary) 
+VALUES
+  (1,"Sales Lead", "$100,000" ),
+  (2,"Salesperson", "$80,000"),
+  (3,"Lead Engineer", "$150,000"),
+  (4,"Software Engineer", "$120,000"),
+  (5. "Account Manager", "$160,000"),
+  (6. "Accountant", "$125,000"),
+  (7. "Legal Team Lead", "$250,000"),
+  (8. "Lawyer", "$190,000");
+
 CREATE TABLE employee(
-  id INTEGER PRIMARY KEY,
+  PRIMARY KEY (id),
   first_name VARCHAR(30),
   last_name VARCHAR(30),
-  role_id INTEGER,
-  manager_id INTEGER
+  role_id INTEGER NULL,
+  manager_id INTEGER NULL,
+  id INTEGER AUTO_INCREMENT
+);
+
+CREATE TABLE managers (
+  PRIMARY KEY (managers_id),
+  manager VARCHAR(30),
+  manager_id INTEGER AUTO_INCREMENT
 );
---
+++ server.js
@@ -0,0 +1,84 @@
+// Import and require mysql2
+const mysql = require('mysql2');
+// inquirer@^8.0.0
+const inquirer = require("inquirer");
+const consoleTables = require("console.table");
+const Connection = require('mysql2/typings/mysql/lib/Connection');
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // {TODO: Add your MySQL password}
+    password: '',
+    database: 'employee_db'
+  },
+  console.log(`Connected to the inventory_db database.`)
+);
+
+var roles = [];
+var employees = [];
+var managers = [];
+
+// GET Role
+const getRole = () => {
+    connection.query(`SELECT title, role_id FROM role`, (err, res) => {
+        if(err) throw err;
+        roles = [];
+        for (let i = 0; i < res.length; i++) {
+            const id = res(i).role_id;
+            const title = res(i).title_id;
+            var newRole = {
+                name: title,
+                value: id
+            }
+            managers.push(newRole);
+        }
+        // console.log(managers)
+        return managers;
+        // console.log(managers)
+    });
+};
+
+// GET Employees
+const getEmployee = () => {
+    connection.query(`SELECT first_name, last_name, id FROM employee`, (err, res) => {
+        if(err) throw err;
+        employees = [];
+        for (let i = 0; i < res.length; i++) {
+            const first_name = res(i).first_name;
+            const last_name = res(i).last_name;
+            var newEmployees = {
+                name: first_name.concat("", lastName),
+                value: id
+            }
+            employees.push(newEmployees);
+        }
+        // console.log(newEmployees)
+        return employees;
+        // console.log(newEmployees)
+    });
+};
+
+// GET Manager
+const getManager = () => {
+    connection.query(`SELECT manager, manager_id FROM managers`, (err, res) => {
+        if(err) throw err;
+        managers = [];
+        for (let i = 0; i < res.length; i++) {
+            const manager = res(i).manager;
+            const manager_id = res(i).manager_id;
+            var newManager = {
+                name: manager,
+                value: manager_id
+            }
+            managers.push(newManager);
+        }
+        // console.log(managers)
+        return managers;
+        // console.log(managers)
+    });
+};
+
