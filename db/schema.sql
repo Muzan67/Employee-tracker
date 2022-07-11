67a6f59c497b9394701e687d07dab8027c630297
Alejandro Cortez <102841726+Muzan67@users.noreply.github.com>
2022-07-11 10:48:09 -0400

    new push

--- db/schema.sql
+++ db/schema.sql
@@ -17,12 +17,12 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  role_id INTEGER,
-  manager_id INTEGER,
-  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
-  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+ id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+ first_name VARCHAR(30),
+ last_name VARCHAR(30),
+ role_id INT UNSIGNED NOT NULL,
+ manager_id INT UNSIGNED NOT NULL,
+ CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+ CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
 );
 
